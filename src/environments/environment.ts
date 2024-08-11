f265d1e33611e50de5c0c98cd4b8ed6acb1e558a
iq-GauravBeniwal <gaurav.beniwal@indoqubix.com>
2024-08-10 21:08:21 -0700

    proxy service testing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,15 @@
 
 export const environment = {
   production: false,
-  BASE_API_PATH : "http://sahosoftweb.com/api/",
-  BASE_IMAGES_PATH : "http://sahosoftweb.com/images/",
-  BASE_USERS_IMAGES_PATH : "http://sahosoftweb.com/users/"
+  // BASE_API_PATH : "http://sahosoftweb.com/api/",
+  // BASE_IMAGES_PATH : "http://sahosoftweb.com/images/",
+  // BASE_USERS_IMAGES_PATH : "http://sahosoftweb.com/users/"
+
+
+BASE_API_PATH: "https://node-proxy-y0s2.onrender.com/api/",
+BASE_IMAGES_PATH: "https://node-proxy-y0s2.onrender.com/images/",
+BASE_USERS_IMAGES_PATH: "https://node-proxy-y0s2.onrender.com/users/",
+
 };
 
 /*
